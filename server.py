2533cd591a99db77bfa524755131c21d3a012d11
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-04-22 22:13:18 -0400

    Bugfixes and some testing
    The testclient will get removed here very soon. I just wanted to use it to test the messages were working. It does seem like I'm not able to get the messages on the terminal yet, but that's for future Vicky to handle

--- server.py
+++ server.py
@@ -9,15 +9,11 @@ from game import Game
 connected = {}
 game = Game()
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-
 async def chat(websocket, message):
     # Send the chat message to everybody except the person who sent it
     others = connected
     del others[message['user']]
-    await websockets.broadcast(others, message)
+    websockets.broadcast(list(others.values()), json.dumps(message))
 
 async def error(websocket, message):
     await websocket.send(message)
@@ -27,21 +23,24 @@ async def join(websocket, message):
         await error(websocket, json.dumps({ 'type': 'error', 'message': 'Error: User already connected' }) )
     else:
         connected[message['user']] = websocket
-        websockets.broadcast(connected.values, json.dumps({ 'type': 'join', 'user': message['user'] }) )
+        print(message)
+        print(connected)
+        websockets.broadcast(list(connected.values()), json.dumps({ 'type': 'join', 'user': message['user'] }) )
 
 async def handler(websocket):
     async for message in websocket:
         event = json.loads(message)
 
         if event['type'] == 'chat':
-            await chat(websocket, message)
+            print(event)
+            await chat(websocket, event)
         
         if event['type'] == 'join':
-            await join(websocket, message)
+            await join(websocket, event)
 
 
 async def main():
-    async with serve(echo, '0.0.0.0', 42069):
+    async with serve(handler, '0.0.0.0', 42069):
         await asyncio.Future()
 
 asyncio.run(main())
---
+++ testclient.py
@@ -0,0 +1,36 @@
+import asyncio
+import websockets
+import json
+
+username = input("Pick a username: ")
+
+async def listen(websocket):
+    async for message in websocket:
+        event = json.loads(message)
+
+        if event['type'] == 'join':
+            if event['user'] == username:
+                print("You have connected")
+            else:
+                print(event['user'] + " has connected")
+        
+        if event['type'] == 'chat':
+            print(event['message'])
+
+async def chat(websocket):
+    while True:
+        chat_message = input("Enter message: ")
+        await websocket.send(json.dumps( { 'type': 'chat', 'user': username, 'message': chat_message } ))
+
+async def main():
+    uri = 'ws://localhost:42069'
+
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps( {'type': 'join', 'user': username} ))
+        await asyncio.gather(
+            listen(websocket),
+            chat(websocket),
+        )
+
+if __name__ == "__main__":
+    asyncio.run(main())
